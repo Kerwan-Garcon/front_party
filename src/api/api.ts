93a42716ab94f5335fe781b548e2f558c524dcbb
Kerwan <77628932+Kerwan-Garcon@users.noreply.github.com>
2024-06-28 23:44:24 +0200

    fast redirect to prod

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useCookies } from "next-client-cookies";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://back-party.vercel.app/",
 });
 
 const useAxiosConfig = () => {
